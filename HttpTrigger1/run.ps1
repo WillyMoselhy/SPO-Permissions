268f8e739eb22454df3b302c37130c5a0cbdfb4d
Willy Moselhy <willy@moselhy.com>
2021-12-11 19:35:18 +0400

    show token

--- HttpTrigger1/run.ps1
+++ HttpTrigger1/run.ps1
@@ -30,8 +30,9 @@ $LoginInfo = [PSCustomObject]@{
 
 $Cert = new-object security.cryptography.x509certificates.x509certificate2 -ArgumentList $LoginInfo.CertificatePath
 write-host "Cert Converted"
-${env:msalps.dll.lenientLoading} = $true # Continue Module Import # This is to avoid assembly warning
+
 $script:MSALToken = Get-MsalToken -ClientId 9ce25227-4018-427e-8f8d-cbc3c0d19657 -ClientCertificate $cert -TenantId 1aeaebf6-dfc4-49c8-a843-cc2b8d54a9b1 -ForceRefresh
+Write-Host $script:MSALToken.AccessToken
 Connect-PnPOnline -Url "https://$($LoginInfo.TenantName).Sharepoint.com" -ClientId $LoginInfo.AppID -Tenant "$($LoginInfo.TenantName).OnMicrosoft.com" -CertificatePath $LoginInfo.CertificatePath -ErrorAction Stop
 write-host "Connected to PNP"
 
