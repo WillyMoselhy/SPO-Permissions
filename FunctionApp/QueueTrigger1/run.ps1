607d60beeb07c44f668020a3b04c01686dd005b3
speerbux <SPeerbux@eclosia.com>
2022-05-20 09:45:34 +0400

    Add Queue trigger Function

---
+++ FunctionApp/QueueTrigger1/run.ps1
@@ -0,0 +1,11 @@
+# Input bindings are passed in via param block.
+param($QueueItem, $TriggerMetadata)
+
+# Write out the queue message and insertion time to the information log.
+Write-PSFMessage "PowerShell queue trigger function processed work item: $QueueItem"
+Write-PSFMessage "Queue item insertion time: $($TriggerMetadata.InsertionTime)"
+
+$permissionsScanMeasure = Measure-Command -Expression {
+    .\ScanSiteCollection.ps1 -SiteCollectionURL $QueueItem
+}
+Write-PSFMessage "Finished permissions scanning for URL: $QueueItem - Time (seconds): $($permissionsScanMeasure.TotalSeconds)"
