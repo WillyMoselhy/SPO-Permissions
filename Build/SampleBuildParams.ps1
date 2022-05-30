35087bca8e7bbf1500bc30519ee200672e1aa35e
Willy Moselhy [MSFT] <willy@moselhy.com>
2022-05-30 09:50:20 +0200

    Update yml and Build

---
+++ Build/SampleBuildParams.ps1
@@ -0,0 +1,16 @@
+$params = @{
+    Location             = 'EastUS'
+    RGName               = 'SPOPermission-RG-01'
+    FunctionAppName      = 'func-SPOPerm-01-20220530'
+    StorageAccountName   = 'safuncspoperm0120220530'
+    KeyVaultName         = 'kvSPOPerm0120220530'
+    PnPApplicationName   = "func-ecl-SPOPerm-01-PnPApp"
+    LogAnalyticsMaxLevel = 5
+}
+$Location             = 'EastUS'
+$RGName               = 'SPOPermission-RG-01'
+$FunctionAppName      = 'func-SPOPerm-01-20220530'
+$StorageAccountName   = 'safuncspoperm0120220530'
+$KeyVaultName         = 'kvSPOPerm0120220530'
+$PnPApplicationName   = "func-ecl-SPOPerm-01-PnPApp"
+$LogAnalyticsMaxLevel = 5
