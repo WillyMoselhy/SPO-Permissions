83b1b2154d7c2b691abbc58113a82bed03ddf151
Willy Moselhy [MSFT] <willy@moselhy.com>
2022-05-30 09:50:06 +0200

    Update Yml and Build Files

--- Build/Build.ps1
+++ Build/Build.ps1
@@ -1,12 +1,17 @@
 #region: Create Azure Resources
 # Parameters
-$Location = 'SouthAfricaNorth'
-$RGName = 'RGP-ECLOSIA-SPOREP-PROD-01'
-$FunctionAppName = 'func-ecl-SPOPerm-01'
-$StorageAccountName = 'saeclfuncspoperm0122'
-$KeyVaultName = 'funcecpSPOPerm01kv22516'
-$PnPApplicationName = "func-ecl-SPOPerm-01-PnPApp"
-$LogAnalyticsMaxLevel = 5
+# To manually run each part of the script, just populate the variables in a separate PS1 than use it here. See SampleBuildParams.ps1 for reference.
+# Running this script requires elevated permissions if you are registering a new PnP Azure AD App in order to handle the certificate.
+param(
+    $Location ,
+    $RGName ,
+    $FunctionAppName ,
+    $StorageAccountName ,
+    $KeyVaultName ,
+    $PnPApplicationName ,
+    $LogAnalyticsMaxLevel
+)
+
 # Validate inputs
 if ($KeyVaultName.Length -gt 24) { Throw "Keyvault name too long" }
 if ($StorageAccountName.Length -gt 24) { Throw "Keyvault name too long" }
@@ -18,41 +23,45 @@ $azContext = Set-AzContext -SubscriptionObject $subscription
 
 # Get tenant information
 $tenantId = $azContext.Tenant.Id
-$azTenant = Get-AzTenant -TenantId $tenantId 
+$azTenant = Get-AzTenant -TenantId $tenantId
 $rootDomain = $azTenant.Domains | Where-Object { $_ -match "^(?<RootDomain>[a-z0-9]*)\.onmicrosoft\.com$" }
 if ($rootDomain.count -eq 1) {
     $rootDomain = $matches.RootDomain
 }
 else {
-    throw "More than one root doamin found!"
+    throw "More than one root domain found!"
 }
 $defaultDomain = $azTenant.DefaultDomain
 $sharePointDomain = "$rootDomain.sharepoint.com"
 
-# We will use this to assign keyvault permissions to the account running the deployment.
+# We will use this to assign KeyVault permissions to the account running the deployment.
 $azAccount = Get-AzADUser -UserPrincipalName $azContext.Account
 
 #region: Setup PnP PowerShell
 Import-Module PnP.PowerShell
 
-$pnpSerivcePrincipal = Get-AzADServicePrincipal -DisplayName $PnPApplicationName
+$pnpServicePrincipal = Get-AzADServicePrincipal -DisplayName $PnPApplicationName
 
-if ($null -eq $pnpSerivcePrincipal) {
-    Write-PSFMessage -Message  "Registering PnP Application"
-    $pnpSerivcePrincipal = Register-PnPAzureADApp -ApplicationName $PnPApplicationName -Tenant $defaultDomain -Interactive -ErrorAction Stop
-    $certBase64 = $pnpSerivcePrincipal.Base64Encoded
-    $pnpClientID = $pnpSerivcePrincipal.'AzureAppId/ClientId'
+if ($null -eq $pnpServicePrincipal) {
+    Write-PSFMessage -Message "Registering PnP Application"
+    # Checking if running as administrator
+    if ([Security.Principal.WindowsIdentity]::GetCurrent().Groups -notcontains 'S-1-5-32-544' ) {
+        throw "Registring PnP Azure Application requires elevated permissions. Please rerun as admin."
+    }
+    $pnpServicePrincipal = Register-PnPAzureADApp -ApplicationName $PnPApplicationName -Tenant $defaultDomain -Interactive -ErrorAction Stop
+    $certBase64 = $pnpServicePrincipal.Base64Encoded
+    $pnpClientID = $pnpServicePrincipal.'AzureAppId/ClientId'
 }
-else {
-    Write-PSFMessage -Message  "PnP App is already registered: $PnPApplicationName"
+else { # THIS PART IS NOT WORKING AS EXPECTED - just recreate the pnp application
+    Write-PSFMessage -Message "PnP App is already registered: $PnPApplicationName"
     $certBase64 = [system.Convert]::ToBase64String(([System.IO.File]::ReadAllBytes('.\func-ecl-SPOPerm-01-PnPApp.pfx')))
-    $pnpClientID = $pnpSerivcePrincipal.AppId
-    # TODO: Check the certificates and make sure this part works properly    
+    $pnpClientID = $pnpServicePrincipal.AppId
+    # TODO: Check the certificates and make sure this part works properly
 }
 
 
 # Create Resource Group
-$null = New-AzResourceGroup -Name $RGName -Location $Location -Force 
+$null = New-AzResourceGroup -Name $RGName -Location $Location -Force
 
 # Deploy function app resources
 $deploymentParams = @{
@@ -65,7 +74,7 @@ $deploymentParams = @{
     AZTenantDefaultDomain = $defaultDomain
     SharePointDomain      = $sharePointDomain
     LogAnalyticsMaxLevel  = $LogAnalyticsMaxLevel
-    
+
     # For SharePoint PnP Module
     PnPApplicationName    = $PnPApplicationName
     PnPClientID           = $pnpClientID
@@ -82,7 +91,7 @@ $msiID = $bicepDeployment.Outputs.msiID.Value
 # Update permissions for MSI(s) to access key vault
 
 $appId = (Get-AzADServicePrincipal -ObjectId $msiID).AppId
-$keyVaultRoleName = 'Key Vault Secrets User' 
+$keyVaultRoleName = 'Key Vault Secrets User'
 if (-Not (Get-AzRoleAssignment -Scope $bicepDeployment.Outputs.keyvaultId.Value -RoleDefinitionName $keyVaultRoleName -ObjectId $msiID)) {
     New-AzRoleAssignment -ApplicationId $appId -RoleDefinitionName $keyVaultRoleName -Scope $bicepDeployment.Outputs.keyvaultId.Value
 }
@@ -93,8 +102,8 @@ else {
 # Update permissions for the storage account output blob container
 $storageRoleName = "Storage Blob Data Contributor"
 if (-Not (Get-AzRoleAssignment -Scope $bicepDeployment.Outputs.outputContainerId.Value -RoleDefinitionName $storageRoleName -ObjectId $msiID)) {
-    Write-PSFMessage -Message  "Assigning role $storageRoleName to Function App MSI $msiID"
-    New-AzRoleAssignment -Scope $bicepDeployment.Outputs.outputContainerId.Value -RoleDefinitionName $storageRoleName -ApplicationId $appId 
+    Write-PSFMessage -Message "Assigning role $storageRoleName to Function App MSI $msiID"
+    New-AzRoleAssignment -Scope $bicepDeployment.Outputs.outputContainerId.Value -RoleDefinitionName $storageRoleName -ApplicationId $appId
 }
 else {
     "Permission is already applied"
@@ -110,7 +119,9 @@ $publishProfile = Get-AzWebAppPublishingProfile -ResourceGroupName $RGName -Name
 $publishProfile | Set-Clipboard
 Read-Host "Function App Publish Profile is in clipboard, please paste it as a new GitHub Secrets"
 # Now we need to edit the yml file to publish the function app
-
+$ymlFile = Get-Content -Path .\.github\workflows\deploy_spopermissions.yml
+$ymlFile = $ymlFile -replace "app-name: '.+'","app-name: '$FunctionAppName'"
+$ymlFile | Set-Content -Path .\.github\workflows\deploy_spopermissions.yml
 #endregion
 
 #region: Import PnP Certificate to Keyvault
@@ -125,7 +136,7 @@ Connect-MgGraph -Scopes Application.ReadWrite.All, Directory.Read.All, Directory
 
 $GraphAppId = "00000003-0000-0000-c000-000000000000"
 $graphSP = Get-MgServicePrincipal -Search "AppId:$GraphAppId" -ConsistencyLevel eventual
-$msiSP = Get-MgServicePrincipal -ServicePrincipalId  $msiID # This is obtained while deploying the function RG, if not get it from Azure Portal. Note: there is one for dev and one for prod
+$msiSP = Get-MgServicePrincipal -ServicePrincipalId $msiID # This is obtained while deploying the function RG, if not get it from Azure Portal. Note: there is one for dev and one for prod
 $msGraphPermissions = @(
     'Directory.Read.All' #Used to read user and group permissions
 )
