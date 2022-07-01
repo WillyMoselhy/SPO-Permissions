ad09a4da72ab73416a111daee07232d7aba65037
Willy Moselhy [MSFT] <willy@moselhy.com>
2022-07-01 21:38:05 +0300

    Logic for no security groups

--- FunctionApp/Modules/SPOPermissionReport/1.0.0/internal/functions/Get-SPOSharePointGroupMember.ps1
+++ FunctionApp/Modules/SPOPermissionReport/1.0.0/internal/functions/Get-SPOSharePointGroupMember.ps1
@@ -13,12 +13,21 @@ function Get-SPOSharePointGroupMember {
     else {
         # Group is not cached
         $pnpGroupMembers = Get-PnPGroupMember -Group $LoginName
+
+        $users = ($pnpGroupMembers | Where-Object { $_.PrincipalType -eq 'User' }).UserPrincipalName
+        if($pnpGroupMembers | Where-Object { $_.PrincipalType -eq 'SecurityGroup' }){
+            $securityGroups = ($pnpGroupMembers | Where-Object { $_.PrincipalType -eq 'SecurityGroup' }).LoginName -replace ".*([\da-zA-Z]{8}-([\da-zA-Z]{4}-){3}[\da-zA-Z]{12}).*", '$1'
+        }
+        else{
+            $securityGroups = $null
+        }
+
         $groupMembers = [PSCustomObject]@{
             # For users we are interested in UserPrincipalName
-            Users          = ($pnpGroupMembers | Where-Object { $_.PrincipalType -eq 'User' }).UserPrincipalName
+            Users          = $users
 
             # For groups, we take the GUID, this will be used to get the members from MS Graph
-            SecurityGroups = ($pnpGroupMembers | Where-Object { $_.PrincipalType -eq 'SecurityGroup' }).LoginName -replace ".*([\da-zA-Z]{8}-([\da-zA-Z]{4}-){3}[\da-zA-Z]{12}).*", '$1'
+            SecurityGroups =  $securityGroups
         }
         Write-PSFMessage -Message "Members added to cache. {0} Users and {1} Security Groups" -StringValues $groupMembers.Users.Count, $groupMembers.SecurityGroups.Count
         #Update cache
